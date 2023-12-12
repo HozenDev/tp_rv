63db40b2578b7a4edb5b2019e1c092d623e15691
Enzo DUREL <endurel@local.isima.fr>
2023-12-12 10:06:33 +0100

    recette aléatoire, ajout texte ??

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -4,12 +4,57 @@ using UnityEngine;
 
 public class Ingredient
 {
+    private int _numberOfTypes = 6;
+    private Type type;
+
     public enum Type
     {
-        FROMAGE = 0, PAINB, PAINH, SALADE, STEAK, TOMATE
+        PAINB = 0, PAINH, SALADE, STEAK, TOMATE, FROMAGE
     };
 
-    private Type type;
+    public Ingredient()
+    {
+        type = (Type) Random.Range(2, _numberOfTypes-1);
+    }
+
+    public Ingredient(Type type)
+    {
+        this.type = type;
+    }
+
+    override
+    public string ToString()
+    {
+        string s = "";
+
+        switch(type)
+        {
+            case Type.FROMAGE:
+                s = "Fromage";
+                break;
+            case Type.PAINB:
+                s = "Pain (Base)";
+                break;
+            case Type.PAINH:
+                s = "Pain (Haut)";
+                break;
+            case Type.SALADE:
+                s = "Salade";
+                break;
+            case Type.STEAK:
+                s = "Steak";
+                break;
+            case Type.TOMATE:
+                s = "Tomate";
+                break;
+            default:
+                // do nothing
+                break;
+        }
+
+        return s;
+    }
+
     public Type getType()
     {
         return type;
--- Assets/Scripts/VerifyBurger.cs
+++ Assets/Scripts/VerifyBurger.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class VerifyBurger : MonoBehaviour
 {
     private Recipe recipe;
+    public int _numberOfIngredients = 6;
+    
+    GameObject _recipeDisplayer;
+    Transform _recipeDisplayerTransform;
 
-    void GetRandomRecipe()
+    void ComputeRandomRecipe()
     {
-        
+        recipe.RandomRecipe(_numberOfIngredients);
+    }
+
+    void afficher()
+    {
+        Debug.Log(recipe);
+    }
+
+    void VerifyRecipe()
+    {
+        // get all ingredients game objects
+        // compare to the recipe
+        // treatment
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Creation Recette
+        recipe = new Recipe();
+        ComputeRandomRecipe();
+        afficher();
+
+        // Creation canva
+        _recipeDisplayer = GameObject.Find("EcranRecette");
+        _recipeDisplayerTransform = _recipeDisplayer.GetComponent<Transform>();
     }
 
     // Update is called once per frame
